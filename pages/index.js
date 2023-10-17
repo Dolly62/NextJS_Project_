3a4b6289ad6eff495c1738c94de3b27cfceb4162
Dolly62 <kambojdolly798@gmail.com>
2023-10-17 13:52:48 +0530

    Use of Mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 // import { useEffect, useState } from "react";
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_MEETUPS = [
@@ -44,11 +45,30 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   //fetch data from an API
+
+  const client = await MongoClient.connect(
+    "mongodb+srv://test-user_12:vgW_4Sy23@cluster0.3uvvbbl.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  console.log(meetups);
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
-    revalidate: 1
+    revalidate: 1,
   };
 }
 
